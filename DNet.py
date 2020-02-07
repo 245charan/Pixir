e3ee98879daefcb79c38f9c6447c3442b259fbcc
Sung-in Lee <plutorian131@gmail.com>
2020-02-08 01:26:04 +0900

    main 테스트 완료, DNet 수정

--- DNet.py
+++ DNet.py
@@ -1,32 +1,42 @@
+import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import initializers
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
-from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, Flatten, LeakyReLU, concatenate, BatchNormalization
 
 
 class DNet(keras.Model):
-    def __init__(self, vector_size):
+    def __init__(self, text_vector):
         super(DNet, self).__init__()
-        self.conv1 = Conv2D(16, 3, padding='same', activation='relu')
-        self.pool1 = MaxPool2D()
-        self.conv2 = Conv2D(32, 3, padding='same')
-        self.pool2 = MaxPool2D()
-        self.flat = Flatten()
-        self.dense1 = Dense(vector_size, activation='relu')
+        self.text_vector = text_vector
+        self.conv1 = Conv2D(64, 5, padding='same', strides=2)
+        self.leakyrelu1 = LeakyReLU()
+        self.bn1 = BatchNormalization()
         self.dropout1 = Dropout(0.3)
-        self.dense2 = Dense(vector_size)
+        self.conv2 = Conv2D(128, 5, padding='same', strides=2)
+        self.leakyrelu2 = LeakyReLU()
+        self.bn2 = BatchNormalization()
         self.dropout2 = Dropout(0.3)
+        self.flat = Flatten()
+        self.dense1 = Dense(128)
+        self.leakyrelu3 = LeakyReLU()
+        self.dropout3 = Dropout(0.3)
+        self.dense2 = Dense(1, activation='tanh')
 
     def call(self, input, **kwargs):
         x = self.conv1(input)
-        x = self.pool1(x)
+        x = self.leakyrelu1(x)
+        x = self.bn1(x)
+        x = self.dropout1(x)
         x = self.conv2(x)
-        x = self.pool2(x)
+        x = self.leakyrelu2(x)
+        x = self.bn2(x)
+        x = self.dropout2(x)
         x = self.flat(x)
         x = self.dense1(x)
-        x = self.dropout1(x)
+        x = self.leakyrelu3(x)
+        x = tf.concat((x, self.text_vector), axis=1)
+        x = self.dropout3(x)
         x = self.dense2(x)
-        x = self.dropout2(x)
 
         return x
 
