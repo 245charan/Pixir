a0441ed09fabc3d5e81916a5f0df3a81c62d8ce6
chojiwon1727 <56914237+chojiwon1727@users.noreply.github.com>
2020-02-20 18:18:48 +0900

    Update tf_to_tflite.py

--- tf_to_tflite.py
+++ tf_to_tflite.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 # Convert the model to tflite model format
 saved_model_dir = './saved_model'
 converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
-                                       tf.lite.OpsSet.SELECT_TF_OPS]
+# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
+#                                        tf.lite.OpsSet.SELECT_TF_OPS]
 tflite_model = converter.convert()
 open('converted_model.tflite', 'wb').write(tflite_model)
