a5de4d7aac84e29f86c65432d65f46fb4bc7edde
Sung-in Lee <plutorian131@gmail.com>
2020-03-02 14:51:32 +0900

    StackGAN stage1 WGAN-GP

--- StackGAN/stage1_no_vae.py
+++ StackGAN/stage1_no_vae.py
@@ -11,7 +11,7 @@ from PIL import Image
 from tensorflow.keras import Input, Model
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import Dense, LeakyReLU, BatchNormalization, ReLU, Reshape, UpSampling2D, Conv2D, \
-    Activation, concatenate, Flatten, Lambda, Concatenate
+    Activation, concatenate, Flatten, Lambda, Concatenate, Conv2DTranspose
 from tensorflow.keras.optimizers import Adam
 
 
@@ -119,7 +119,7 @@ def load_dataset(filenames_file_path, class_info_file_path, cub_dataset_dir, emb
 def generate_c(x):  # CA(Conditional Augment) Network
     mean = x[:, :128]
     log_sigma = x[:, 128:]
-    stddev = tf.exp(log_sigma)
+    stddev = tf.exp(log_sigma / 2)
     epsilon = tf.random.truncated_normal(tf.constant((mean.shape[1], ), dtype='int32'))
     c = stddev * epsilon + mean
     return c
@@ -143,58 +143,60 @@ def build_embedding_compressor_model():
     return model
 
 
-def build_stage1_generator():
+def build_stage1_generator(embedding_compressor):
     """Stage 1 생성기 모델 빌드"""
     input_layer = Input(shape=(1024, ))
-    x = Dense(256)(input_layer)
-    mean_logsigma = LeakyReLU(0.2)(x)
-    c = Lambda(generate_c)(mean_logsigma)
+    # x = Dense(128)(input_layer)
+    c = embedding_compressor(input_layer)
+    # c = Lambda(generate_c)(mean_logsigma)
 
     input_layer2 = Input(shape=(100,))
 
     gen_input = Concatenate(axis=1)([c, input_layer2])
 
     x = Dense(128 * 8 * 4 * 4, use_bias=False)(gen_input)
-    x = ReLU()(x)
 
     x = Reshape((4, 4, 128 * 8), input_shape=(128 * 8 * 4 * 4, ))(x)
-
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(512, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(256, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
+    x = Conv2DTranspose(512, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
+    # x = UpSampling2D(size=(2, 2))(x)
+    # x = Conv2D(512, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(128, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
+    x = Conv2DTranspose(256, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
+    # x = UpSampling2D(size=(2, 2))(x)
+    # x = Conv2D(256, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(64, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
+    x = Conv2DTranspose(128, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
+    # x = UpSampling2D(size=(2, 2))(x)
+    # x = Conv2D(128, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = Conv2D(3, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
+    # x = Conv2DTranspose(64, kernel_size=3, strides=2, padding='same', use_bias=False)(x)
+    # x = UpSampling2D(size=(2, 2))(x)
+    # x = Conv2D(64, kernel_size=3, padding='same', strides=1, use_bias=False)(x)
+    # x = BatchNormalization()(x)
+    # x = ReLU()(x)
+
+    x = Conv2DTranspose(3, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
+    # x = Conv2D(3, kernel_size=1, padding='same', strides=1, use_bias=False)(x)
     x = Activation(activation='tanh')(x)
 
-    stage1_gen = Model(inputs=[input_layer, input_layer2], outputs=[x, mean_logsigma])
+    stage1_gen = Model(inputs=[input_layer, input_layer2], outputs=[x])
 
     return stage1_gen
 
 
-def build_stage1_discriminator():
+def build_stage1_discriminator(embedding_compressor):
     input_layer = Input(shape=(64, 64, 3))
 
-    x = Conv2D(64, kernel_size=4, strides=2, padding='same', input_shape=(64, 64, 3), use_bias=False)(input_layer)
-    x = LeakyReLU(0.2)(x)
-
-    x = Conv2D(128, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
-    x = BatchNormalization()(x)
+    x = Conv2D(128, kernel_size=4, strides=2, padding='same', input_shape=(64, 64, 3), use_bias=False)(input_layer)
     x = LeakyReLU(0.2)(x)
 
     x = Conv2D(256, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
@@ -205,18 +207,22 @@ def build_stage1_discriminator():
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
 
+    x = Conv2D(1024, kernel_size=4, strides=2, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.2)(x)
+
     input_layer2 = Input(shape=(1024, ))
-    ecm = build_embedding_compressor_model()
-    ec = ecm(input_layer2)
+    # ecm = build_embedding_compressor_model()
+    ec = embedding_compressor(input_layer2)
     ec = Reshape((1, 1, 128))(ec)
     ec = tf.tile(ec, (1, 4, 4, 1))
 
     merged_input = concatenate([x, ec])
 
-    x2 = Conv2D(64 * 8, kernel_size=1, strides=1, padding='same')(merged_input)
+    x2 = Conv2D(1024, kernel_size=1, strides=1, padding='same')(merged_input)
     x2 = BatchNormalization()(x2)
     x2 = LeakyReLU(0.2)(x2)
-    x2 = Conv2D(1, kernel_size=4)(x2)
+    x2 = Conv2D(1, kernel_size=4, strides=4)(x2)
     x2 = Reshape((1, ))(x2)
     # x2 = Flatten()(x2)
     # x2 = Dense(1)(x2)
@@ -248,6 +254,7 @@ def KL_loss(y_true, y_pred):
     loss = tf.square(mean) + tf.exp(logsigma) - logsigma - 1
     # loss = -logsigma + .5 * (-1 + tf.exp(2. * logsigma) + tf.square(mean))
     loss = 0.5 * tf.reduce_sum(loss)
+
     return loss
 
 
@@ -266,36 +273,47 @@ def save_rgb_img(img, path):
     plt.close()
 
 
+def plot_generated_images(epoch, images, dim=(10, 10), figsize=(10, 10)):
+    images = (images + 1) * 0.5
+    plt.figure(figsize=figsize)
+    for i in range(dim[0] * dim[1]):
+        plt.subplot(dim[0], dim[1], i + 1)
+        plt.imshow(images[i], interpolation='nearest')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('gan_generated_image_epoch_%d.png' % epoch)
+
+
 if __name__ == '__main__':
     # gen = build_stage1_generator()
-    disc = build_stage1_discriminator()
+    # disc = build_stage1_discriminator()
     # gen.summary()
-    disc.summary()
+    # disc.summary()
     # gan = build_adversarial_model(gen, disc)
     # gan.summary()
-    # data_dir = '../data/birds'
-    # train_dir = data_dir + '/train'
-    # test_dir = data_dir + '/test'
-    # image_size = 64
-    # batch_size = 64
-    # z_dim = 100
-    # stage1_generator_lr = 0.0002
-    # stage1_discriminator_lr = 0.0002
-    # stage1_lr_decay_step = 600
-    # epochs = 1000
-    # condition_dim = 128
-    #
-    # embeddings_file_path_train = train_dir + '/char-CNN-RNN-embeddings.pickle'
-    # embeddings_file_path_test = test_dir + '/char-CNN-RNN-embeddings.pickle'
-    #
-    # filenames_file_path_train = train_dir + '/filenames.pickle'
-    # filenames_file_path_test = test_dir + '/filenames.pickle'
-    #
-    # class_info_file_path_train = train_dir + '/class_info.pickle'
-    # class_info_file_path_test = test_dir + '/class_info.pickle'
-    #
-    # cub_dataset_dir = data_dir + '/CUB_200_2011'
-    #
+    data_dir = '../data/birds'
+    train_dir = data_dir + '/train'
+    test_dir = data_dir + '/test'
+    image_size = 64
+    batch_size = 64
+    z_dim = 100
+    stage1_generator_lr = 0.0002
+    stage1_discriminator_lr = 0.0002
+    stage1_lr_decay_step = 600
+    epochs = 1000
+    condition_dim = 128
+
+    embeddings_file_path_train = train_dir + '/char-CNN-RNN-embeddings.pickle'
+    embeddings_file_path_test = test_dir + '/char-CNN-RNN-embeddings.pickle'
+
+    filenames_file_path_train = train_dir + '/filenames.pickle'
+    filenames_file_path_test = test_dir + '/filenames.pickle'
+
+    class_info_file_path_train = train_dir + '/class_info.pickle'
+    class_info_file_path_test = test_dir + '/class_info.pickle'
+
+    cub_dataset_dir = data_dir + '/CUB_200_2011'
+
     # X_train, y_train, embeddings_train = load_dataset(filenames_file_path_train,
     #                                                   class_info_file_path_train,
     #                                                   cub_dataset_dir,
@@ -307,99 +325,113 @@ if __name__ == '__main__':
     #                                                cub_dataset_dir,
     #                                                embeddings_file_path_test,
     #                                                image_size=(64, 64))
-    #
+
     # np.save('X_train.npy', X_train)
     # np.save('X_test.npy', X_test)
-    #
-    # gen_optimizer = Adam(lr=stage1_generator_lr, beta_1=0.5, beta_2=0.999)
-    # disc_optimizer = Adam(lr=stage1_discriminator_lr, beta_1=0.5, beta_2=0.999)
-    #
+    X_train = np.load('X_train.npy')
+    X_text = np.load('X_test.npy')
+    embeddings_train = np.load('bert_embeddings_train.npy')
+    embeddings_test = np.load('bert_embeddings_test.npy')
+
+    gen_optimizer = Adam(lr=stage1_generator_lr, beta_1=0.5, beta_2=0.999)
+    disc_optimizer = Adam(lr=stage1_discriminator_lr, beta_1=0.5, beta_2=0.999)
+
     # ca_model = build_ca_model()
     # ca_model.compile(loss='binary_crossentropy', optimizer='adam')
-    #
-    # stage1_gen = build_stage1_generator()
-    # stage1_gen.compile(loss='binary_crossentropy', optimizer=gen_optimizer)
-    #
-    # stage1_disc = build_stage1_discriminator()
-    # stage1_disc.compile(loss='binary_crossentropy', optimizer=disc_optimizer)
-    #
-    # embedding_compressor_model = build_embedding_compressor_model()
-    # embedding_compressor_model.compile(loss='binary_crossentropy', optimizer='adam')
-    #
-    # adversarial_model = build_adversarial_model(stage1_gen, stage1_disc)
-    # adversarial_model.compile(loss=['binary_crossentropy', KL_loss], loss_weights=[1, 2.0],
-    #                           optimizer=gen_optimizer, metrics=None)
-    #
-    # tensorboard = TensorBoard(log_dir='logs/'.format(time.time()))
-    # tensorboard.set_model(stage1_gen)
-    # tensorboard.set_model(stage1_disc)
+
+    embedding_compressor_model = build_embedding_compressor_model()
+    embedding_compressor_model.compile(loss='binary_crossentropy', optimizer='adam')
+
+    stage1_gen = build_stage1_generator(embedding_compressor_model)
+    stage1_gen.compile(loss='binary_crossentropy', optimizer=gen_optimizer)
+
+    stage1_disc = build_stage1_discriminator(embedding_compressor_model)
+    stage1_disc.compile(loss=tf.nn.sigmoid_cross_entropy_with_logits, optimizer=disc_optimizer)
+
+    adversarial_model = build_adversarial_model(stage1_gen, stage1_disc)
+    adversarial_model.compile(loss=[tf.nn.sigmoid_cross_entropy_with_logits, KL_loss],
+                              optimizer=gen_optimizer, metrics=None)
+
+    tensorboard = TensorBoard(log_dir='logs/'.format(time.time()))
+    tensorboard.set_model(stage1_gen)
+    tensorboard.set_model(stage1_disc)
     # tensorboard.set_model(ca_model)
     # tensorboard.set_model(embedding_compressor_model)
-    #
-    # real_labels = np.ones((batch_size, 1), dtype=np.float32) * 0.9
-    # fake_labels = np.zeros((batch_size, 1), dtype=np.float32) + 0.1
-    #
-    # summary_writer = tf.summary.create_file_writer('./logs')
-    #
-    # for epoch in range(epochs):
-    #     print('===========================================')
-    #     print('Epoch is:', epoch)
-    #     print('Number of batches:', int(X_train.shape[0] / batch_size))
-    #
-    #     gen_losses = []
-    #     disc_losses = []
-    #
-    #     number_of_batches = int(X_train.shape[0] / batch_size)
-    #     for index in range(number_of_batches):
-    #         print('Batch: {}'.format(index + 1))
-    #
-    #         z_noise = np.random.normal(0, 1, size=(batch_size, z_dim))
-    #         image_batch = X_train[index * batch_size:(index + 1) * batch_size]
-    #         embedding_batch = embeddings_train[index * batch_size:(index + 1) * batch_size]
-    #
-    #         # 이미지 정규화
-    #         image_batch = (image_batch - 127.5) / 127.5
-    #
-    #         fake_images, _ = stage1_gen.predict([embedding_batch, z_noise], verbose=3)
-    #
-    #         compressed_embedding = embedding_compressor_model.predict_on_batch(embedding_batch)
-    #         compressed_embedding = np.reshape(compressed_embedding, (-1, 1, 1, condition_dim))
-    #         compressed_embedding = np.tile(compressed_embedding, (1, 4, 4, 1))
-    #
-    #         # 진짜 이미지/가짜 이미지/잘못된 이미지로 판별기 훈련
-    #         disc_loss_real = stage1_disc.train_on_batch([image_batch, compressed_embedding],
-    #                                                     np.reshape(real_labels, (batch_size, 1)))
-    #         disc_loss_fake = stage1_disc.train_on_batch([fake_images, compressed_embedding],
-    #                                                     np.reshape(fake_labels, (batch_size, 1)))
-    #         disc_loss_wrong = stage1_disc.train_on_batch([image_batch[:(batch_size - 1)], compressed_embedding[1:]],
-    #                                                      np.reshape(fake_labels[1:], (batch_size-1, 1)))
-    #
-    #         # 생성기 훈련
-    #         g_loss = adversarial_model.train_on_batch([embedding_batch, z_noise, compressed_embedding],
-    #                                                   [np.ones((batch_size, 1)) * 0.9, np.ones((batch_size, 256)) * 0.9])
-    #
-    #         # 손실 저장
-    #         d_loss = 0.5 * np.add(disc_loss_real, 0.5 * np.add(disc_loss_wrong, disc_loss_fake))
-    #         print('d_loss:', d_loss)
-    #         print('g_loss:', g_loss)
-    #         disc_losses.append(d_loss)
-    #         gen_losses.append(g_loss)
-    #
-    #     # 손실값 텐서보드 저장
-    #     with summary_writer.as_default():
-    #         tf.summary.scalar('generator_loss', np.mean(gen_losses)[0], step=epoch)
-    #         tf.summary.scalar('discriminator_loss', np.mean(disc_losses), step=epoch)
-    #
-    #     # 에포크 별 이미지 생성, 저장
-    #     z_noise2 = np.random.normal(0, 1, size=(batch_size, z_dim))
-    #     embedding_batch = embeddings_test[0:batch_size]
-    #     fake_images, _ = stage1_gen.predict_on_batch([embedding_batch, z_noise2])
-    #
-    #     for i, img in enumerate(fake_images[:10]):
-    #         save_rgb_img(img, f'results/gen_{epoch}_{i}.png')
-    #
-    # stage1_gen.save_weights('stage1_gen.h5')
-    # stage1_disc.save_weights('stage1_disc.h5')
+
+    real_labels = np.ones((batch_size, 1), dtype=np.float32) * 0.9
+    fake_labels = np.zeros((batch_size, 1), dtype=np.float32) + 0.1
+
+    summary_writer = tf.summary.create_file_writer('./logs')
+
+    for epoch in range(epochs):
+        print('===========================================')
+        print('Epoch is:', epoch)
+        print('Number of batches:', int(X_train.shape[0] / batch_size))
+
+        gen_losses = []
+        disc_losses = []
+
+        number_of_batches = int(X_train.shape[0] / batch_size)
+        for index in range(number_of_batches):
+            print('Batch: {}'.format(index + 1))
+
+            z_noise = np.random.normal(0, 1, size=(batch_size, z_dim))
+            image_batch = X_train[index * batch_size:(index + 1) * batch_size]
+            embedding_batch = embeddings_train[index * batch_size:(index + 1) * batch_size]
+
+            # 이미지 정규화
+            image_batch = (image_batch - 127.5) / 127.5
+
+            fake_images, _ = stage1_gen.predict([embedding_batch, z_noise], verbose=3)
+
+            # compressed_embedding = embedding_compressor_model.predict_on_batch(embedding_batch)
+            # compressed_embedding = np.reshape(compressed_embedding, (-1, 1, 1, condition_dim))
+            # compressed_embedding = np.tile(compressed_embedding, (1, 4, 4, 1))
+
+            # 진짜 이미지/가짜 이미지/잘못된 이미지로 판별기 훈련
+            disc_loss_real = stage1_disc.train_on_batch([image_batch, embedding_batch],
+                                                        np.reshape(real_labels, (batch_size, 1)))
+            disc_loss_fake = stage1_disc.train_on_batch([fake_images, embedding_batch],
+                                                        np.reshape(fake_labels, (batch_size, 1)))
+            disc_loss_wrong = stage1_disc.train_on_batch([image_batch[:(batch_size - 1)], embedding_batch[1:]],
+                                                         np.reshape(fake_labels[1:], (batch_size-1, 1)))
+
+            # 생성기 훈련
+            g_loss = adversarial_model.train_on_batch([embedding_batch, z_noise],
+                                                      [np.ones((batch_size, 1)) * 0.9, np.ones((batch_size, 256)) * 0.9])
+
+            # 손실 저장
+            d_loss = 0.5 * np.add(disc_loss_real, 0.5 * np.add(disc_loss_wrong, disc_loss_fake))
+            print('d_loss:', d_loss)
+            print('g_loss:', g_loss)
+            disc_losses.append(d_loss)
+            gen_losses.append(g_loss)
+
+        # 손실값 텐서보드 저장
+        with summary_writer.as_default():
+            tf.summary.scalar('generator_loss', np.mean(gen_losses), step=epoch)
+            tf.summary.scalar('discriminator_loss', np.mean(disc_losses), step=epoch)
+
+        # 10에포크 별 이미지 생성, 저장
+        if epoch == 0 or (epoch + 1) % 10 == 0:
+            z_noise2 = np.random.normal(0, 1, size=(batch_size, z_dim))
+            embedding_batch = embeddings_test[0:batch_size]
+            fake_images, _ = stage1_gen.predict_on_batch([embedding_batch, z_noise2])
+
+            plot_generated_images(epoch + 1, fake_images, dim=(8, 8))
+
+        if (epoch + 1) % 100 == 0:
+            stage1_gen.save_weights(f'stage1_gen_epoch{epoch + 1}.h5')
+            stage1_disc.save_weights(f'stage1_disc_epoch{epoch + 1}.h5')
+
+            stage1_generator_lr *= 0.5
+            stage1_discriminator_lr *= 0.5
+            gen_optimizer.learning_rate.assign(stage1_generator_lr)
+            disc_optimizer.leaning_rate.assign(stage1_discriminator_lr)
+            print('learning rate decayed!!')
+
+    stage1_gen.save_weights('stage1_gen.h5')
+    stage1_disc.save_weights('stage1_disc.h5')
 
 
 
---
+++ data/coco_korean/bert_test.py
@@ -0,0 +1,45 @@
+import numpy as np
+import tensorflow as tf
+from keras_bert import get_checkpoint_paths, load_trained_model_from_checkpoint
+# import sys
+# sys.path.append('../bert')
+# from modeling import BertConfig, BertModel
+
+# config = BertConfig(30797)
+# config.from_json_file('../bert_eojeol/bert_config.json')
+
+inputs = np.load('coco_korean_tokens.npy')
+# inputs = tf.convert_to_tensor(tokens, dtype=tf.int32)
+print(inputs.shape)
+print(inputs[:2])
+
+segments = np.ones_like(inputs)
+
+# model = BertModel(config, False, inputs)
+
+ckpt = '../bert_eojeol/'
+# model = tf.keras.Model()
+# checkpoint = tf.train.Checkpoint(model=model)
+# checkpoint.restore(tf.train.latest_checkpoint(ckpt))
+# print(model.layers)
+
+paths = get_checkpoint_paths(ckpt)
+model = load_trained_model_from_checkpoint(paths.config, paths.checkpoint, training=False, seq_len=142)
+# model.summary()
+# model.save('koreanbert.h5')
+
+dataset = tf.data.Dataset.from_tensor_slices((inputs, segments))
+dataset = dataset.batch(5000)
+
+outputs = np.empty((inputs.shape[0], 768))
+i = 0
+for data in dataset:
+    inp = data[0]
+    seg = data[1]
+    output = model.predict([inp, seg], steps=123287)
+    cls_output = output[:, 0, :]
+    outputs[i * 5000:(i + 1) * 5000] = cls_output
+    i += 1
+print(outputs.shape)
+print(outputs[:2])
+np.save('korean_embedding2.npy', outputs)
